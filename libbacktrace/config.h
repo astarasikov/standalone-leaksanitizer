6c690934b262595eb75ae72374604b45d230a27a
Alexander Tarasikov <a.tarasikov@undev.ru>
2014-03-18 21:08:47 +0400

    Hack to build libbacktrace locally

---
+++ libbacktrace/config.h
@@ -0,0 +1,13 @@
+#ifndef __BT_CONFIG_H__
+#define __BT_CONFIG_H__
+
+#define HAVE_FCNTL 1
+
+#include <stdint.h>
+
+#include "backtrace.h"
+#include "internal.h"
+
+#define BACKTRACE_ELF_SIZE 64
+
+#endif
